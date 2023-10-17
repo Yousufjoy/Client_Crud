a5f19726ea672e3ec4a8aad5043f5a6060032488
Yousufjoy <kanekiken333@gmail.com>
2023-10-17 14:37:05 +0600

    when i post some information from front end to backend it receives

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import "./App.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
       <h1>Simple CRUD!</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <input type="submit" value="Add User" />
+        <br />
+      </form>
     </>
   );
 }
